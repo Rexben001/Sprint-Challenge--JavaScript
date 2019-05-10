584e8fae1d67a18d725877304a65a3888228da62
Rexben <rexben.rb@gmail.com>
2019-05-10 09:46:59 +0100

    attempt functions

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,32 +1,45 @@
-// ==== Callbacks ====  
+// ==== Callbacks ====
 
 /* Step 1: Create a higher-order function that accepts a callback
-  * Create a higher-order function named consume that can take 3 parameters.
-  * The first two parameters can accept any argument
-  * The last parameter accepts a callback 
-  * In the body of the function return the callback with the two parameters that you created
-*/
+ * Create a higher-order function named consume that can take 3 parameters.
+ * The first two parameters can accept any argument
+ * The last parameter accepts a callback
+ * In the body of the function return the callback with the two parameters that you created
+ */
 
+function consume(params1, params2, params3) {
+  return console.log(params3(params1, params2));
+}
 
 /* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
+ * Create a function named add that returns the sum of two numbers
+ * Create a function named multiply that returns the product of two numbers
+ * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+ */
 
+function add(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function greeting(name, name2) {
+  return `Hello ${name} ${name2}, nice to meet you!`
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
-
-// ==== Closures ==== 
+// ==== Closures ====
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
-
+//The `internal` variable can be accessed by `nestedfunction()` because of the nestedFunction() and internal variable are inside the same block scope.
+// Explanation:
 
 const external = "I'm outside the function";
 
@@ -36,7 +49,7 @@ function myFunction() {
 
   function nestedFunction() {
     console.log(internal);
-  };
+  }
   nestedFunction();
 }
 myFunction();
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -77,7 +77,12 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 
-const universities = graduates.map(university => university.university).sort();
+const universities = [];
+for (let i = 0; i < graduates.length; i++)
+{
+  universities.push(graduates[i].university);
+}
+universities.sort();
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
@@ -86,12 +91,24 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
-const contactInfo = graduates.map(contact => `${contact.first_name} ${contact.email}`);
+const contactInfo = [];
+
+for (let i = 0; i < graduates.length; i++)
+{
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`);
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = universities.filter(uni => uni.includes('Uni'));
+const uni = [];
+for (let i = 0; i < graduates.length; i++)
+{
+  if ((graduates[i]).university.includes('Uni'))
+  {
+    uni.push(graduates[i].university)
+  }
+}
 console.log(uni);
 
 
